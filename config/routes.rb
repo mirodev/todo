32ee55c4cbbe0809a077775c8976285a70fc359f
Miro Slav <miroslav@Miros-MacBook.local>
2016-08-14 18:47:41 +0300

    created the about page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'page/home'
   root 'page#home'
+  get 'about' => 'page#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
